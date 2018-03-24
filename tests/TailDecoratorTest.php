76fbfa3f7c37ae9e51acfa149472a954cfbe55f8
Akira Fujimoto <hr6nnn@gmail.com>
2018-03-25 02:26:49 +0900

    fix

--- tests/TailDecoratorTest.php
+++ tests/TailDecoratorTest.php
@@ -4,7 +4,7 @@ namespace Akrfjmt\Decorated\Tests;
 
 use Akrfjmt\Decorated\Decorated;
 use Akrfjmt\Decorated\Tests\Util\Cat;
-use Akrfjmt\Decorated\YankeeDecorator;
+use Akrfjmt\Decorated\TailDecorator;
 use PHPUnit\Framework\TestCase;
 
 
@@ -12,20 +12,22 @@ class YankeeDecoratorTest extends TestCase
 {
     public function testDecorate()
     {
-        $yankeeDecorator = new YankeeDecorator();
+        $yankeeDecorator = new TailDecorator();
         $cat = new Cat();
 
         /** @var Cat|Decorated $decorated */
         $decorated = new Decorated($cat, $yankeeDecorator);
+        $this->assertSame('abcdef！？', $decorated->Concat('abc', 'def'));
 
         /** @var Cat|Decorated $doubleDecorated */
         $doubleDecorated = new Decorated($cat, $yankeeDecorator, $yankeeDecorator);
+        $this->assertSame('114514！？！？', $doubleDecorated->Concat('114', '514'));
 
         /** @var Cat|Decorated $tripleDecorated */
         $tripleDecorated = new Decorated($cat, $yankeeDecorator, $yankeeDecorator, $yankeeDecorator);
 
-        $this->assertSame('abcdef！？', $decorated->Concat('abc', 'def'));
-        $this->assertSame('114514！？！？', $doubleDecorated->Concat('114', '514'));
+        // test reentrancy
         $this->assertSame('889464！？！？！？', $tripleDecorated->Concat('889', '464'));
+        $this->assertSame('364364！？！？！？', $tripleDecorated->Concat('364', '364'));
     }
 }
